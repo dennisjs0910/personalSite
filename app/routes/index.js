038aa403f33b9360529bd39fa99be1a00d1e6fe4
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-05-10 22:49:40 -0700

    initializing proper database schema is still in progress. removed previous migration as well as dropped previous table after experimenting with knex

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 
+// TODO: can delete in the future
 routes.use("/", require('@routes/sample'));
 // routes.get('/', (req, res) => {
 //   res.send("inside routes/index.js");
