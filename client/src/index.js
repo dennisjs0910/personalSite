2ea11e4e4aa50eb5313504891a9bb74d3ec7e377
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-05-11 17:12:25 -0700

    initialized client/index file. configured redux store and reducers. wtd: actions, home page, etc..

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import * as serviceWorker from "./serviceWorker";
+// import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middlewares = process.env.NODE_ENV !== "production" ? [reduxThunk, logger] : [reduxThunk];
+const store = createStore(reducers, {}, applyMiddleware(...middlewares));
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.querySelector("#root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
