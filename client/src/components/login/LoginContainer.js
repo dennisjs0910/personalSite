e43a1e70069de4b9e839e2ff6eaae0d20a6db569
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-06-11 20:32:21 -0700

    login css

--- client/src/components/login/LoginContainer.js
+++ client/src/components/login/LoginContainer.js
@@ -3,13 +3,21 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import LoginForm from "./LoginForm";
 import { AuthAction } from '../../actions';
+import { Layout } from 'antd';
+import "./login.css";
+
+const { Content } = Layout;
 
 class LoginContainer extends Component {
 
   render() {
     const { loginUser } = this.props;
     return (
-      <LoginForm loginUser={loginUser}/>
+      <Content className="fullscreen">
+        <div className="login-container">
+          <LoginForm loginUser={loginUser}/>
+        </div>
+      </Content>
     );
   }
 }
--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import { Form, Icon, Input, Button } from 'antd';
+import "./login.css";
 
 class LoginFormTemplate extends Component {
 
@@ -18,40 +19,42 @@ class LoginFormTemplate extends Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('email', {
-            rules: [{
-              type: 'email',
-              message: 'The input is not valid E-mail!',
-            },{
-              required: true, message: 'Please input your E-mail!'
-            }],
-            validateTrigger: "onSubmit",
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Email"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
+      <div className="login-form-container">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item className="login-input-container">
+            {getFieldDecorator('email', {
+              rules: [{
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },{
+                required: true, message: 'Please input your E-mail!'
+              }],
+              validateTrigger: "onSubmit",
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Email"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item className="login-input-container password-container">
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     );
   }
 }
