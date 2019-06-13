e1daf88724678940591c79c3f01656519a761389
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-06-12 23:50:09 -0700

    user permission is now by default 'basic', admin users need to be manually created for now

--- server/query_manager/userManager.js
+++ server/query_manager/userManager.js
@@ -37,7 +37,7 @@ module.exports = {
       const result = await knex(USER_TABLE)
         .insert(Object.assign(
           { ...fields }, {
-            permission: "admin",
+            permission: "basic",
             password: encryptedPassword
           })
         );
