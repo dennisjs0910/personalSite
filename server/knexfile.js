14cafe398639b431b876c0a6c602bad85ce688d2
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-06-13 23:11:36 -0700

    fixed typo, got to stop doing this on master

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,8 +3,8 @@ module.exports = {
   client: 'pg',
   connection: {
     host: process.env.POSTGRES_HOST,
-    user: process.env.process.env.POSTGRES_USER,
-    password: process.env.process.env.POSTGRES_PASSWORD,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB
   }
 };
