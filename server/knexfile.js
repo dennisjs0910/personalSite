ec01cd146a2f45b9a355fd2dfe2181762bf1890c
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-06-16 22:50:28 -0700

    setup env variable for server and changed connection files to reflect change in env names

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,9 +2,9 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
   }
 };
