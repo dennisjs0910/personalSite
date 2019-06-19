3e737906962fbd948de55a205f71147733263c57
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-06-19 01:03:29 -0700

    adding folder paths for prod and dev env

--- server/routes/imageUpload.js
+++ server/routes/imageUpload.js
@@ -3,21 +3,26 @@ const cloudinary = require('cloudinary');
 
 routes.post('/', (req, res) => {
   const values = Object.values(req.files);
-  const promises = values.map(image => cloudinary.uploader.upload(image.path));
+  const promises = values.map(image => cloudinary.v2.uploader.upload(image.path, {
+    use_filename: false,
+    unique_filename: true,
+    folder: `${process.env.CLOUDINARY_FOLDER}/`
+  }));
 
   Promise.all(promises).then(results => {
     res.status(200);
     res.json(results)
   }).catch(err => {
     res.sendStatus(500);
-  })
+  });
 });
 
 
-routes.delete('/:public_id', async (req, res) => {
+
+routes.delete('/', async (req, res) => {
   try {
-    const { public_id } = req.params;
-    const deleteResponse = await cloudinary.uploader.destroy(public_id);
+    const { public_id } = req.body;
+    const deleteResponse = await cloudinary.v2.uploader.destroy(public_id);
     res.sendStatus(204);
   } catch (err) {
     res.sendStatus(404);
