21794b1f289424f1791b9fb91ac5996dd73dace0
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-07-02 21:32:59 -0700

    changed folder name to match convention

--- server/query_manager/index.js
+++
@@ -1,9 +0,0 @@
-const userManager = require('@qm/userManager');
-const blogManager = require('@qm/blogManager');
-const commentManager = require('@qm/commentManager');
-
-module.exports = {
-  userManager,
-  blogManager,
-  commentManager,
-}
